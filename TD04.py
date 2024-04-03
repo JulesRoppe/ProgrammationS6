bdd50af5a50510197c0f5066831e789e44302a5f
JulesRoppe <jules.roppe@gmail.com>
2024-04-03 11:46:09 +0200

    ajout TD04

---
+++ TD04.py
@@ -0,0 +1,76 @@
+""" TD04 """
+import matplotlib.pyplot as plt
+import os
+
+print(os.listdir())
+print([None for i in range(10)] + [None for i in range(10)])
+with open("frenchssaccent.dic", 'r') as f:
+    text = f.read()
+lexique = text.split("\n")
+
+class Hashtable:
+    def __init__(self, hash, N):
+        self.hash = hash
+        self.table = []
+        for i in range(N):
+            self.table.append(None)
+
+    def put(self, key, value):
+        index = self.hash(key)%len(self.table)
+        if self.table[index] == None:
+            self.table[index] = [(key, value)]
+        else:
+            counter = 0
+            for i in range(len(self.table[index])):
+                if self.table[index][i][0] == key:
+                    self.table[index][i] = (key, value)
+                    counter = 1
+            if counter == 0:
+                self.table[index].append((key, value))
+        return self
+
+    def get(self, key):
+        index = self.hash(key)%len(self.table)
+        if self.table[index] == None:
+            return None
+        else:
+            return self.table[index][0][1]
+
+    def repartition(self):
+        y = [0 for i in range(len(self.table))]
+        N = len(y)
+        x = range(N)
+        for index in range(N):
+            if self.table[index] != None:
+                y[index] = len(self.table[index])
+        width = 1 / 1.5
+        plt.bar(x, y, width, color="blue")
+        plt.show()
+
+    def resize(self):
+        self.table = [None for i in range(2*len(self.table))]
+        return self.table
+            
+def hash_naive(key):
+    h = 0
+    for i in key:
+        h += ord(i)
+    return h
+
+def hash_better(key):
+    return sum([ord(key[i]) * 33**i for i in range(len(key))])
+
+if __name__ == '__main__':
+    print(hash_naive('abc'))
+    ht = Hashtable(hash_better, 320)
+    nombre_element = 0
+    for i in range(len(lexique)):
+        mot = lexique[i]
+        ht.put(mot, len(mot))
+        if i > 1.2*len(ht.table):
+            ht.resize()
+            i = 0
+
+    ht.repartition()
+
+
