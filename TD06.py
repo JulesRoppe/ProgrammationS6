e886028772117114d0a56364ec844d70334167e2
JulesRoppe <jules.roppe@gmail.com>
2024-05-15 23:09:46 +0200

    ajout TD06

--- TD06.py
+++ TD06.py
@@ -12,6 +12,7 @@ can = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg='white')
 
 graph = [[2, 7, 3], [3, 4, 9, 10], [5, 8, 0], [10, 1, 4, 6],
 [3, 1, 6], [2], [3, 10, 4], [0], [2], [10, 1], [3, 1, 6, 9]]
+graph2 = [[1, 2],[0],[0]]
 
 pos = np.array([(random.random()*WIDTH, random.random()*HEIGHT) for i in range(len(graph))])
 vit = np.array([((random.random() - 0.5), (random.random() - 0.5)) for i in range(len(graph))])
@@ -23,39 +24,44 @@ class Unknown:
 
     def ressort(self, event):
         can.delete("all")
-        self.vit = self.vit + np.array([(somme_forces(i)[0]*tau, somme_forces(i)[1]*tau) for i in range(len(graph))])
-        self.pos = self.pos + tau * vit
-        draw(can, graph, self.pos)
+        self.vit = self.vit + np.array([(self.somme_forces(i)[0]*tau, self.somme_forces(i)[1]*tau) for i in range(len(graph))])
+        self.pos = self.pos + tau * self.vit
+        self.draw(can, graph, self.pos)
 
-def draw(can, graph, pos):
-    for i in range(len(graph)):
-        for j in graph[i]:  # sucs de i a j
-            can.create_line(pos[i][0], pos[i][1], pos[j][0], pos[j][1])
-    for (x, y) in pos:
-        can.create_oval(x-4,y-4,x+4,y+4,fill="black")
-    return
+    def force(self, i, j):
+        l = np.sqrt((self.pos[i][0] - self.pos[j][0]) ** 2 + (self.pos[i][1] - self.pos[j][1]) ** 2) + 0.001
+        F = k * (l - l0)
+        vect_u = ((self.pos[j][0] - self.pos[i][0]) / l, (self.pos[j][1] - self.pos[i][1]) / l)
+        return (F, vect_u)
 
-draw(can, graph, pos)
+    def somme_forces(self, i):
+        somme_f = [0, 0]
+        for j in range(len(graph[i])):
+            somme_f[0] += (self.force(i, j)[0] * self.force(i, j)[1][0])
+            somme_f[1] += (self.force(i, j)[0] * self.force(i, j)[1][1])
+        return somme_f
+
+    def draw(self, can, graph, pos):
+        can.create_rectangle(100, 100, 200, 200, fill="red")
+        barycentre_x, barycentre_y = 0, 0
+        for i in range(len(graph)):
+            barycentre_x += self.pos[i][0]/len(graph)
+            barycentre_y += self.pos[i][1]/len(graph)
+            for j in graph[i]:
+                can.create_line(self.pos[i][0], self.pos[i][1], self.pos[j][0], self.pos[j][1])
+        for (x, y) in pos:
+            can.create_oval(x-4,y-4,x+4,y+4,fill="black")
+        can.move("all", -(barycentre_x-WIDTH/2), -(barycentre_y-HEIGHT/2))
+        return
+
 
 k = 0.2
 l0 = 100
 tau = 0.1
-def force(i, j):
-    l = np.sqrt((pos[i][0] - pos[j][0])**2 + (pos[i][1] - pos[j][1])**2)+0.001
-    F = k*(l - l0)
-    vect_u = ((pos[j][0] - pos[i][0])/l, (pos[j][1] - pos[i][1])/l)
-    return (F, vect_u)
+if __name__ == '__main__':
+    Master = Unknown(pos, vit)
+    Master.draw(can, graph, pos)
 
-def somme_forces(i):
-    somme_f = [0,0]
-    for j in range(len(graph[i])):
-
-        somme_f[0] += (force(i, j)[0]*force(i, j)[1][0])
-        somme_f[1] += (force(i, j)[0]*force(i, j)[1][1])
-        print(somme_f)
-    return somme_f
-
-Master = Unknown(pos, vit)
 root.bind("<f>", Master.ressort)
 
 can.grid(row=0, column=0)
